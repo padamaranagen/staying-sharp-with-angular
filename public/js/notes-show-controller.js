2e71ea8011e9b0662a08350eae4b87c1ebe18e17
Nagendra Prasad <nagendramca2011@gmail.com>
2016-07-14 14:49:32 +0530

    Task:
    1. Inject the routeParams service into the NotesShowController so that we get access to the id in the URL.
    2. Inject the $http service into the NotesShowController so we can use it to fetch a note.
    3. Use the $http() function to fetch a note from the /notes/:id URL. Upon success, save the resulting data to a note variable on the controller.

--- public/js/notes-show-controller.js
+++ public/js/notes-show-controller.js
@@ -1,4 +1,8 @@
 ﻿angular.module('NoteWrangler')
-.controller('NotesShowController', [function () {
+.controller('NotesShowController', ['$routeParams', '$http', function ($routeParams, $http) {
+    var controller = this;
 
+    $http({ method: 'GET', url: '/notes/' + $routeParams.id }).success(function (data) {
+        controller.note = data;
+    });
 }]);
