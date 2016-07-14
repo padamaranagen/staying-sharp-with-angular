dc364d40828c2ac4ae6f2ae1b22f393cc9541f5d
Nagendra Prasad <nagendramca2011@gmail.com>
2016-07-14 12:46:58 +0530

    Task:
    1. Create a controller function inside this /notes route.
    2. Give the newly created controller an alias of notesCtrl so that we can access it from within the notes index template.
    3. We'll need to make a call to the server to retrieve the list of notes data, so we'll need to use the $http service in the controller we just created. Inject the $http service into the controller.
    4. Use the $http() function to make an HTTP request to the /notes URL. Upon success, save the resulting data to a notes variable on the controller.

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,27 @@
 ﻿var app = angular.module("NoteWrangler", ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider
+         .when('/', {
+             redirectTo: '/users'
+         })
         .when('/notes', {
-            templateUrl: '/templates/pages/notes/index.html'
+            templateUrl: '/templates/pages/notes/index.html',
+            controller: function ($http) {
+                var controller = this;
+                $http({ method: 'GET', url: '/notes' }).success(function (data) {
+                    controller.notes = data;
+                });
+            },
+            controllerAs: 'notesCtrl'
         })
         .when('/users', {
             templateUrl: '/templates/pages/users/index.html'
         })
         .when('/notes/new', {
             templateUrl: 'templates/pages/notes/edit.html'
+        })       
+        .otherwise({
+            redirectTo: '/notes'
         })
-        .when('/', {
-            redirectTo: '/users'
-        })
-        .otherwise({ redirectTo: '/notes' })
     ;
 });
