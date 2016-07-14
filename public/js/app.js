f5de8048a68274895c31ffb20afbb4c8eee57695
Nagendra Prasad <nagendramca2011@gmail.com>
2016-07-14 11:41:44 +0530

    added notes and user pages

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("NoteWrangler", ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/notes', {
+            templateUrl: '/templates/pages/notes/index.html'
+        })
+        .when('/users', {
+            templateUrl: '/templates/pages/users/index.html'
+        })
+    ;
+});
