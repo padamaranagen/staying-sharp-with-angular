8e417b1fc47a233cc0df38c3a6ab471d1afc7c03
Nagendra Prasad <nagendramca2011@gmail.com>
2016-07-14 13:03:53 +0530

    added new page and it's controller

---
+++ public/js/notes-create-controller.js
@@ -0,0 +1,6 @@
+﻿angular.module('NoteWrangler')
+.controller('NotesCreateController', [function () {
+    this.saveNote = function (note) {
+        $http({ method: 'POST', url: '/notes', data: note });
+    };
+}]);
